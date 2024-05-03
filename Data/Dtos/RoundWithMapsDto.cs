ded6b1bdcea32b9e586ab550a644a5bd5d89e8f7
andyret26 <andyret26@gmail.com>
2024-05-03 14:02:39 +0200

    :wrench: Updated RoundWithMapsDto

--- Data/Dtos/RoundWithMapsDto.cs
+++ Data/Dtos/RoundWithMapsDto.cs
@@ -4,6 +4,7 @@ public class RoundWithMapsDto
 {
     public int Id { get; set; }
     public string Name { get; set; } = null!;
+    public bool IsQualifier { get; set; }
     public List<MapDto>? Mappool { get; set; }
     public List<MapSuggestionDto>? MapSuggestions { get; set; }
 }
