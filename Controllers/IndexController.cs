cb673133f419313da473f6720a5660c41dd31191
Anders Wiik <anders.wiik@horizon56.io>
2024-08-16 10:31:11 +0200

    Added ping endpoint

--- Controllers/IndexController.cs
+++ Controllers/IndexController.cs
@@ -1,14 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
+using OtmApi.Services.HostService;
 
 namespace OtmApi.Controllers;
 
 [Route("/")]
 [ApiController]
-public class IndexController : ControllerBase
+public class IndexController(IHostService hostService) : ControllerBase
 {
+    private readonly IHostService _hostService = hostService;
+
     [HttpGet]
     public IActionResult Index()
     {
         return Ok("Welcome to the OtmApi!");
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Ping()
+    {
+        await _hostService.GetByIdAsync(3191010);
+        return Ok();
     }
 }
